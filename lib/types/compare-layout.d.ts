60f867c9a40e783728a566d050ca3e782b41aa14
rpill <rpill@github.com>
2023-06-14 09:53:36 +0000

    047e4a1ef78ba8c120a76c0a20b5df7033da4745

---
+++ lib/types/compare-layout.d.ts
@@ -0,0 +1,13 @@
+import { Page } from 'puppeteer';
+import { Options as BrowserOptions } from './utils/puppeteer';
+export interface Options {
+    browserOptions: BrowserOptions;
+    fullPage?: boolean;
+    canonicalImage?: string;
+    reqPercentage?: number;
+}
+type BeforeScreenshotHandler = (page: Page) => Promise<void>;
+export interface Handler {
+    onBeforeScreenshot?: BeforeScreenshotHandler | null;
+}
+export {};
---
+++ lib/types/run-tests.d.ts
@@ -0,0 +1,6 @@
+import { Resource } from "i18next";
+export interface Options {
+    projectPath: string;
+    lang: string;
+    resource: Resource;
+}
